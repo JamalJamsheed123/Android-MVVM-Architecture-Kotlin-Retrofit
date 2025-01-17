8a4d996910dd86b28a2c6c1d35f2e12502870fb7
JamalJamsheed123~ <jamal.jamsheed2@gmail.com>
2025-01-18 01:42:05 +0500

    Update Project
    Update Api Data to get ID and Body from JsonPlaceholder API also Update UI as well.

--- app/src/main/java/com/example/android_mvvm_architecture_kotlin_retrofit/Adapter/PostAdapter.kt
+++ app/src/main/java/com/example/android_mvvm_architecture_kotlin_retrofit/Adapter/PostAdapter.kt
@@ -6,19 +6,24 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_mvvm_architecture_kotlin_retrofit.Model.Post
+import com.example.android_mvvm_architecture_kotlin_retrofit.R
 
 class PostAdapter(private val posts: List<Post>) : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
     class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val titleTextView: TextView = view.findViewById(android.R.id.text1)
+        val idTextView: TextView = view.findViewById(R.id.tvid)
+        val titleTextview: TextView = view.findViewById(R.id.textTitle)
+        val bodyTextView: TextView = view.findViewById(R.id.textBody)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.user_info_layout, parent, false)
         return PostViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        holder.titleTextView.text = posts[position].title
+        holder.idTextView.text = posts[position].id.toString()
+        holder.titleTextview.text = posts[position].title
+        holder.bodyTextView.text = posts[position].body
     }
 
     override fun getItemCount(): Int = posts.size
